f1a3bc719504f0e04c79fc53778d5c7f48b52e35
DuyHieu2004 <doanhieu.11052004@gmail.com>
2026-04-29 18:18:32 +0700

    refactor: update url connect to endpoints
    Co-authored-by: NguyenHuuThinh2410 <nguyenhuuthinh2410@gmail.com>

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -8,4 +8,15 @@ class ApiConstants {
   static const String registerEndpoint = '$authEndpoint/register';
   static const String changePasswordEndpoint = '$authEndpoint/change-password';
   static const String logoutEndpoint = '$authEndpoint/logout';
+  static const String lopHocEndpoint = '$baseUrl/api/LopHoc';
+  static const String khoaHocEndpoint = '$baseUrl/api/KhoaHoc';
+  static const String dangKyEndpoint = '$baseUrl/api/DangKy';
+  static const String registerClassEndpoint = '$dangKyEndpoint/register';
+  static const String ketQuaThiEndpoint = '$baseUrl/api/KetQuaThi';
+  static const String ketQuaHocTapEndpoint = '$baseUrl/api/KetQuaHocTap';
+  static const String phongThiEndpoint = '$baseUrl/api/PhongThi';
+  static const String thanhToanEndpoint = '$baseUrl/api/ThanhToan';
+  static const String giangVienEndpoint = '$baseUrl/api/GiangVien';
+  static const String chuyenLopEndpoint = '$baseUrl/api/ChuyenLop';
+  static const String chungChiEndpoint = '$baseUrl/api/ChungChi';
 }
